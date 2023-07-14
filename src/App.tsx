07e24dadde0713238acdb1a5086c7b1a8e6f7c1e
wreckingralph06 <ralphwaldocandaza@gmail.com>
2023-07-14 12:14:17 +0800

    Moved Content section into a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,11 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
 import Navbar from './components/Navbar';
+import Content from './components/Content';
 
 function App() {
-  const [randomData, setRandomData] = useState<any[]>([]);
-  const [randomTriggered, setRandomTriggered] = useState<Boolean>(false);
-  const [loading, setLoading] = useState<Boolean>(false);
-
-  const generateRandom = () => {
-    setRandomTriggered(true);
-  }
-
-  useEffect(() => {
-    let ignore = false;
-    Axios.get('https://randomuser.me/api')
-      .then(res => {
-        if (!ignore) {
-          setRandomData(res.data.results)
-        }
-      }).catch(err => console.log(err))
-    return () => { ignore = true }
-  }, []);
-
-  useEffect(() => {
-    if (randomData.length > 0) {
-      setLoading(true)
-    }
-  }, [randomData]);
-
-  useEffect(() => {
-    if (randomTriggered) {
-      Axios.get('https://randomuser.me/api')
-        .then(res => {
-          setRandomData(res.data.results)
-        }).catch(err => console.log(err))
-      setRandomTriggered(false);
-    }
-  }, [randomTriggered])
-
   return (
     <div className="App">
       <Navbar />
-      <div className="content">
-        {loading ?
-          <div className="details">
-            <div className="details-img-section">
-              <img className="details-img" src={randomData[0].picture.large}></img>
-            </div>
-            <div className="details-text-section">
-              <div className="detail-text">
-                <h1>{randomData[0].name.first + ' ' + randomData[0].name.last}</h1>
-              </div>
-              <div className="detail-text">
-                <img className="label-icon" src="/email-1-svgrepo-com.svg"></img>
-                <div className="label-value">{randomData[0].email}</div>
-              </div>
-            </div>
-            <button className="refresh-button" onClick={generateRandom}>
-              <span style={{ color: "white" }}>Refresh</span>
-            </button>
-          </div>
-          : <></>}
-      </div>
+      <Content />
     </div >
   );
 }
